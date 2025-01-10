15d7ecdb0cfbb838126d4cd8c774fab90d1735af
rts-Liva <U2451012@rts-365.dk>
2025-01-10 14:07:51 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pagesPlugin from 'vite-plugin-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pagesPlugin()],
 })
